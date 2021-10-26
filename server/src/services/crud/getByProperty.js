5ee61f77e59223d6780a06432e0eaeb626cc71df
demetreades <demetreades@protonmail.com>
2021-10-26 16:13:44 +0300

    typo fixed

--- server/src/services/crud/getByProperty.js
+++ server/src/services/crud/getByProperty.js
@@ -4,13 +4,13 @@ const { StatusCodes } = require('http-status-codes');
 const { BaseError } = require('../../utils');
 
 module.exports = async (Model, property, sort = -1) => {
-  const results = await Model.findOne({ property}).sort({
-    createdAt: sort,
-  });
+	const results = await Model.findOne({ property }).sort({
+		createdAt: sort,
+	});
 
-  if (!results) {
-    throw new BaseError('Not found', StatusCodes.NOT_FOUND);
-  }
+	if (!results) {
+		throw new BaseError('Not found', StatusCodes.NOT_FOUND);
+	}
 
-  return results;
+	return results;
 };
