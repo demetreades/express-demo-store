0ccbe0f88ebfab833deae9022ba660298b776499
demetreades <demetreades@protonmail.com>
2021-10-26 21:44:23 +0300

    object removed

--- server/src/services/crud/getByProperty.js
+++ server/src/services/crud/getByProperty.js
@@ -4,7 +4,7 @@ const { StatusCodes } = require('http-status-codes');
 const { BaseError } = require('../../utils');
 
 module.exports = async (Model, property, sort = -1) => {
-	const results = await Model.findOne({ property }).sort({
+	const results = await Model.findOne(property).sort({
 		createdAt: sort,
 	});
 
