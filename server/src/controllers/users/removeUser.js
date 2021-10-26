d6ad8625249dab9f5e6a1ca7ad3f92eb558d788c
demetreades <demetreades@protonmail.com>
2021-10-26 21:41:33 +0300

    BaseError removed

--- server/src/controllers/users/removeUser.js
+++ server/src/controllers/users/removeUser.js
@@ -2,7 +2,7 @@
 
 const { StatusCodes } = require('http-status-codes');
 const asyncHandler = require('express-async-handler');
-const { BaseError, logger } = require('../../utils');
+const { logger } = require('../../utils');
 const userService = require('../../services/crud');
 const { User } = require('../../services/models');
 
