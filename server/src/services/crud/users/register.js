6e5d6c9cd4d839b29167a832b4964e84ae5bde56
demetreades <demetreades@protonmail.com>
2021-10-26 18:19:12 +0300

    typo fix

--- server/src/services/crud/users/register.js
+++ server/src/services/crud/users/register.js
@@ -5,23 +5,23 @@ const { BaseError } = require('../../../utils');
 const { User } = require('../../../services/models');
 
 module.exports = async (body) => {
-	const { name, email, password } = body;
+  const { name, email, password } = body;
 
-	const userExists = await User.findOne({ email });
+  const userExists = await User.findOne({ email });
 
-	if (userExists) {
-		throw new BaseError('User already exists', StatusCodes.BAD_REQUEST);
-	} else {
-		const user = await User.create({
-			name,
-			email,
-			password,
-		});
+  if (userExists) {
+    throw new BaseError('User already exists', StatusCodes.BAD_REQUEST);
+  } else {
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
 
-		if (!user) {
-			throw new BaseError('Invalid user data', StatusCodes.BAD_REQUEST);
-		}
+    if (!user) {
+      throw new BaseError('Invalid user data', StatusCodes.BAD_REQUEST);
+    }
 
-		return user;
-	}
+    return user;
+  }
 };
