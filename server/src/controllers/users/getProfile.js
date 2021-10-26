d2e9438ae5e5a2696eadc514955f8665c8097c2e
demetreades <demetreades@protonmail.com>
2021-10-26 21:40:08 +0300

    getProfile added

---
+++ server/src/controllers/users/getProfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { StatusCodes } = require('http-status-codes');
+const asyncHandler = require('express-async-handler');
+const { logger } = require('../../utils');
+const userService = require('../../services/crud');
+const { User } = require('../../services/models');
+
+module.exports = asyncHandler(async (req, res, next) => {
+
+	const { user: { id } } = req;
+
+  const user = await userService.getByProperty(User, { id });
+
+  logger.info(
+    `USER PROFILE name: ${user.name} id: ${user._id}, email: ${user.email}`
+  );
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: user,
+  });
+});
