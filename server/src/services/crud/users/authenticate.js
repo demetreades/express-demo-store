4cef8581e65afea4b699f7adc66019995f3d7fe9
demetreades <demetreades@protonmail.com>
2021-10-26 17:02:59 +0300

    !user conditional added

--- server/src/services/crud/users/authenticate.js
+++ server/src/services/crud/users/authenticate.js
@@ -1,24 +1,27 @@
 'use strict';
 
 const { StatusCodes } = require('http-status-codes');
-const { BaseError, logger, generateToken } = require('../../../utils');
+const { BaseError, generateToken } = require('../../../utils');
 const { User } = require('../../../services/models');
 
 module.exports = async (body) => {
-	const { email, password } = body;
+  const { email, password } = body;
 
-	const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-	if (user && (await user.matchPassword(password))) {
+  if (!user) {
+    throw new BaseError('User not found', StatusCodes.NOT_FOUND);
+  }
 
-		return {
-			_id: user._id,
-			name: user.name,
-			email: user.email,
-			isAdmin: user.isAdmin,
-			token: generateToken(user._id),
-		};
-	} else {
-		throw new BaseError(StatusCodes.UNAUTHORIZED, 'Invalid email or password');
-	}
+  if (user && (await user.matchPassword(password))) {
+    return {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    };
+  } else {
+    throw new BaseError('Invalid email or password', StatusCodes.UNAUTHORIZED);
+  }
 };
