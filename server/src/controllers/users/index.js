31462bfdc6ebb115a0f3ba4c3d55d342d0b821c2
demetreades <demetreades@protonmail.com>
2021-10-26 21:40:44 +0300

    logOutUser, getUser, getProfile added

--- server/src/controllers/users/index.js
+++ server/src/controllers/users/index.js
@@ -1,15 +1,19 @@
 const authUser = require('./authUser');
+const logOutUser = require('./logOutUser');
 const registerUser = require('./registerUser');
-const getUserProfile = require('./getUserProfile');
+const getUser = require('./getUser');
 const getUsers = require('./getUsers');
+const getProfile = require('./getProfile');
 const removeUser = require('./removeUser');
 const updateUser = require('./updateUser');
 
 module.exports = {
-	authUser,
-	registerUser,
-	getUserProfile,
-	getUsers,
-	removeUser,
-	updateUser,
+  authUser,
+  registerUser,
+	getProfile,
+  getUser,
+  getUsers,
+  removeUser,
+  updateUser,
+	logOutUser,
 };
