502d8bcf2d6a3b35bd142c5c65f2aec60026ef6a
demetreades <demetreades@protonmail.com>
2021-10-26 16:12:50 +0300

    Order model refactored

--- server/src/services/crud/index.js
+++ server/src/services/crud/index.js
@@ -4,10 +4,17 @@ const update = require('./update');
 const getAll = require('./getAll');
 const getByProperty = require('./getByProperty');
 
+const register = require('./users/register');
+const authenticate = require('./users/authenticate');
+
 module.exports = {
 	create,
 	remove,
 	update,
 	getAll,
 	getByProperty,
+	users: {
+		register,
+		authenticate
+	}
 };
