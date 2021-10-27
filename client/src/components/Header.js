09b09cf859104f707911fdf196148b18fad7d95c
demetreades <demetreades@protonmail.com>
2021-10-27 22:21:48 +0300

    conditional links rendering for login signup and dashboard

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,38 +1,60 @@
-import { Link } from 'react-router-dom';
-import { Typography, AppBar, Toolbar } from '@material-ui/core';
-import { Accessibility } from '@material-ui/icons';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
+import { Accessibility } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
+import { useContext } from "react";
 import useStyles from '../styles';
 
 const Header = () => {
-  const classes = useStyles();
+	const { user } = useContext(UserContext);
 
-  return (
-    <>
-      <AppBar position="relative">
-        <Toolbar>
-          <Accessibility className={classes.icon} />
-          <Link to="/home">
-            <Typography variant="h6">demo store</Typography>
-          </Link>
-          <Link
-            to="/dashboard"
-            className={classes.ml5}
-            variant="body2"
-            underline="hover">
-            dashboard
-          </Link>
-          <Link
-            to="/login"
-            className={classes.ml5}
-            variant="body2"
-            underline="hover">
-            login
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+	const classes = useStyles();
+
+	return (
+		<>
+			<AppBar position="relative">
+				<Toolbar>
+					<Accessibility className={classes.icon} />
+					<Link to="/home">
+						<Typography variant="h6">demo store</Typography>
+					</Link>
+					{user.isAdmin ? (
+						<Link
+							to="/dashboard"
+							className={classes.ml5}
+							variant="body2"
+							underline="hover">
+							dashboard
+						</Link>
+					) : ('')
+					}
+
+					{/* <Link
+						to="/login"
+						className={classes.ml5}
+						variant="body2"
+						underline="hover">
+						login
+					</Link> */}
+					{/*  */}
+					<Link to="/login"
+						className={classes.ml5}
+						variant="body2"
+						underline="hover">
+						{user.login ? 'PROFILE' : 'login'}
+					</Link>
+					{!user.login ? (
+						<Link to="/register" className={classes.ml5}>
+							Sign Up
+						</Link>
+					) : (
+						''
+					)}
+				</Toolbar>
+			</AppBar>
+		</>
+	);
 };
 
 export default Header;
