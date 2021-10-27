2a516db37058d8b9a843027d31d312bc26c7894e
demetreades <demetreades@protonmail.com>
2021-10-27 10:27:33 +0300

    typo fixed on exports

--- server/src/services/db/DATA/index.js
+++ server/src/services/db/DATA/index.js
@@ -2,7 +2,7 @@ const { orders } = require('./orders');
 const { products } = require('./products');
 const { users } = require('./users');
 
-module.expots = {
+module.exports = {
   orders,
   products,
   users,
