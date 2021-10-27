ee03f5f9a80bcf424c2ae12d40ffe66d9573482b
demetreades <demetreades@protonmail.com>
2021-10-27 10:26:52 +0300

    seeder refactored

--- server/src/services/db/seeder.js
+++ server/src/services/db/seeder.js
@@ -3,56 +3,53 @@
 require('dotenv').config();
 const { Product, User, Order } = require('../../services/models');
 const { products, users, orders } = require('./DATA');
+const stuff = require('./DATA');
 const { logger } = require('../../utils');
 const dbConnection = require('./connection');
 
 dbConnection();
 
 const importData = async () => {
-	try {
-		// await Order.deleteMany();
-		await Product.deleteMany();
-		await User.deleteMany();
+  console.log('mpike0');
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
 
-		const createdUsers = await User.insertMany(users);
-		const adminUser = createdUsers[0]._id;
+    const createdUsers = await User.insertMany(users);
+    const adminUser = createdUsers[0]._id;
 
-		const sampleProducts = products.map((product) => {
-			return { ...product, user: adminUser };
-		});
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
 
-		// const sampleCategories = categories.map((category) => {
-		//   return { ...category, user: adminUser };
-		// });
+    await Product.insertMany(sampleProducts);
 
-		await Product.insertMany(sampleProducts);
-		// await Category.insertMany(sampleCategories);
-
-		logger.info('Data imported');
-		process.exit();
-	} catch (err) {
-		logger.error(`Seeder Error: ${err}`);
-		process.exit(1);
-	}
+    logger.info('\n\nData imported\n');
+    process.exit();
+  } catch (err) {
+    logger.error(`Seeder Error: ${err}`);
+    console.log(err);
+    process.exit(1);
+  }
 };
 
 const deleteData = async () => {
-	try {
-		// await Order.deleteMany();
-		await Product.deleteMany();
-		// await Category.deleteMany();
-		await User.deleteMany();
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
 
-		logger.info('Data deleted!');
-		process.exit();
-	} catch (err) {
-		logger.error(`${err}`);
-		process.exit(1);
-	}
+    logger.info('\n\nData deleted\n');
+    process.exit();
+  } catch (err) {
+    logger.error(`${err}`);
+    process.exit(1);
+  }
 };
 
 if (process.argv[2] === '-d') {
-	deleteData();
+  deleteData();
 } else {
-	importData();
+  importData();
 }
