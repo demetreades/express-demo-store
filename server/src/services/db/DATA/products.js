173cd8750a2db4512e311d984c0838397dfcfa9c
demetreades <demetreades@protonmail.com>
2021-10-27 10:32:22 +0300

    image property commented

--- server/src/services/db/DATA/products.js
+++ server/src/services/db/DATA/products.js
@@ -2,7 +2,7 @@ exports.products = [
   {
     name: 'Airpods Wireless Bluetooth Headphones',
     slug: 'airpods-wireless-bluetooth-headphones',
-    image: '/img/products/airpods.jpg',
+    // image: '/img/products/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
@@ -13,7 +13,7 @@ exports.products = [
   {
     name: 'iPhone 11 Pro 256GB Memory',
     slug: 'iphone-11-pro-256gb-memory',
-    image: '/img/products/phone.jpg',
+    // image: '/img/products/phone.jpg',
     description:
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     brand: 'Apple',
@@ -24,7 +24,7 @@ exports.products = [
   {
     name: 'Cannon EOS 80D DSLR Camera',
     slug: 'cannon-eos-80d-dslr-camera',
-    image: '/img/products/camera.jpg',
+    // image: '/img/products/camera.jpg',
     description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     brand: 'Cannon',
@@ -35,7 +35,7 @@ exports.products = [
   {
     name: 'Sony Playstation 4 Pro White Version',
     slug: 'sony-playstation-4-pro-white-version',
-    image: '/img/products/playstation.jpg',
+    // image: '/img/products/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     brand: 'Sony',
@@ -46,7 +46,7 @@ exports.products = [
   {
     name: 'Logitech G-Series Gaming Mouse',
     slug: 'logitech-g-series-gaming-mouse',
-    image: '/img/products/mouse.jpg',
+    // image: '/img/products/mouse.jpg',
     description:
       'Get a better handle on your games with this Logitech light sync gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     brand: 'Logitech',
@@ -57,7 +57,7 @@ exports.products = [
   {
     name: 'Amazon Echo Dot 3rd Generation',
     slug: 'amazon-echo-dot-3rd-generation',
-    image: '/img/products/alexa.jpg',
+    // image: '/img/products/alexa.jpg',
     description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     brand: 'Amazon',
