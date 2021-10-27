df190c11f662e17700e94513f1a93932518b38eb
demetreades <demetreades@protonmail.com>
2021-10-27 17:45:44 +0300

    typo fixed

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,25 +3,25 @@ import { Typography } from '@material-ui/core';
 import useStyles from '../styles';
 
 const Footer = () => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <>
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          contacts-app
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="textSecondary"
-          gutterBottom>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          facere quia sunt.
-        </Typography>
-      </footer>
-    </>
-  );
+	return (
+		<>
+			<footer className={classes.footer}>
+				<Typography variant="h6" align="center" gutterBottom>
+					demo-store example footer
+				</Typography>
+				<Typography
+					variant="subtitle1"
+					align="center"
+					color="textSecondary"
+					gutterBottom>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+					facere quia sunt.
+				</Typography>
+			</footer>
+		</>
+	);
 };
 
 export default Footer;
