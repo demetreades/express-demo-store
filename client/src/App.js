4a4e7f413581de61e5a2052226629c5cc6f1ca27
demetreades <demetreades@protonmail.com>
2021-10-28 11:46:06 +0300

    ProtectedRoute added

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,9 @@ import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Missing404 from './pages/Missing404';
-import ProtectedRoute from './components/dashboard/ProtectedRoute';
+import ProtectedRoute from './utils/ProtectedRoute';
 import { BrowserRouter as Router } from 'react-router-dom'
 import SignUp from './pages/SignUp';
-// import Routes from './components/Routes'
 import { UserContext } from './context/UserContext'
 import { useContext } from 'react'
 
@@ -25,7 +24,6 @@ const App = () => {
 						<Route path={['/', '/home']} exact component={HomePage} />
 						<Route path="/login" component={Login} />
 						<Route path="/register" component={SignUp} />
-						{/* <Route path="/dashboard" component={Dashboard} /> */}
 						<ProtectedRoute
 							path="/dashboard"
 							auth={user.isAdmin}
