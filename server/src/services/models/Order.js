f1460895f3ee1f1f252999ab616262617d10da40
demetreades <demetreades@protonmail.com>
2021-10-28 11:48:49 +0300

    firstName lastName added

--- server/src/services/models/Order.js
+++ server/src/services/models/Order.js
@@ -3,62 +3,70 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    orderItems: [
-      {
-        quantity: {
-          type: Number,
-          required: true,
-        },
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true,
-          ref: 'Product',
-        },
-      },
-    ],
-    address: {
-      type: String,
-      required: [true, 'Address is required'],
-    },
-    city: {
-      type: String,
-      required: [true, 'City is required'],
-    },
-    postalCode: {
-      type: Number,
-      required: [true, 'Postal Code is required'],
-    },
-    totalPrice: {
-      type: Number,
-      require: true,
-      default: 0,
-    },
-    isPaid: {
-      type: Boolean,
-      require: true,
-      default: false,
-    },
-    paidAt: {
-      type: Date,
-    },
-    isDelivered: {
-      type: Boolean,
-      require: false,
-      default: false,
-    },
-    deliveredAt: {
-      type: Date,
-    },
-  },
-  {
-    timestamps: true,
-  }
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		firstName: {
+			type: String,
+			required: [true, 'Firstname is required'],
+		},
+		lastName: {
+			type: String,
+			required: [true, 'Lastname is required'],
+		},
+		orderItems: [
+			{
+				quantity: {
+					type: Number,
+					required: true,
+				},
+				product: {
+					type: mongoose.Schema.Types.ObjectId,
+					required: true,
+					ref: 'Product',
+				},
+			},
+		],
+		address: {
+			type: String,
+			required: [true, 'Address is required'],
+		},
+		city: {
+			type: String,
+			required: [true, 'City is required'],
+		},
+		postalCode: {
+			type: Number,
+			required: [true, 'Postal Code is required'],
+		},
+		totalPrice: {
+			type: Number,
+			require: true,
+			default: 0,
+		},
+		isPaid: {
+			type: Boolean,
+			require: true,
+			default: false,
+		},
+		paidAt: {
+			type: Date,
+		},
+		isDelivered: {
+			type: Boolean,
+			require: false,
+			default: false,
+		},
+		deliveredAt: {
+			type: Date,
+		},
+	},
+	{
+		timestamps: true,
+	}
 );
 
 module.exports = mongoose.model('Order', orderSchema);
