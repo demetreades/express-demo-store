10ab413178df291bea4f806c70242516d1167b52
demetreades <demetreades@protonmail.com>
2021-10-28 19:26:41 +0300

    ml5 ml10 cart orderCard productCard child added

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,36 +1,62 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  ml5: {
-    marginLeft: '15px',
-    color: 'white',
-    textDecoration: 'none',
-  },
-  mt5: {
-    marginTop: '25px',
-  },
-  card: {
-    marginTop: '16px',
-  },
-  btn: {
-    fontSize: '42px',
-    backgroundColor: 'violet',
-    '&:hover': {
-      backgroundColor: 'blue',
-    },
-  },
-  title: {
-    marginBottom: '20px',
-  },
-  field: {
-    marginTop: '20px',
-    marginBottom: '20px',
-    display: 'block',
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: '50px 0',
-  },
+	ml5: {
+		margin: ' 0 5px',
+		textDecoration: 'none',
+	},
+	ml10: {
+		margin: '0 10px',
+	},
+	mt5: {
+		marginTop: '25px',
+	},
+	productCard: {
+		margin: '8px',
+		maxWidth: '300px',
+		height: '300px',
+	},
+	btn: {
+		fontSize: '42px',
+		backgroundColor: 'violet',
+		'&:hover': {
+			backgroundColor: 'blue',
+		},
+	},
+	title: {
+		marginBottom: '20px',
+	},
+	field: {
+		marginTop: '20px',
+		marginBottom: '20px',
+		display: 'block',
+	},
+	main: {
+		marginBottom: '80px',
+		display: 'flex',
+		// minHeight: '85.9vh',
+		minHeight: '84vh',
+		flexDirection: 'column',
+	},
+	footer: {
+		position: 'fixed',
+		width: '100%',
+		bottom: '0',
+	},
+	child: {
+		flexGrow: 1,
+		minHeight: 0,
+		overflow: 'scroll',
+	},
+	orderCard: {
+		width: '25%',
+		margin: '12px auto',
+	},
+	cart: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
 }));
 
 export default useStyles;
