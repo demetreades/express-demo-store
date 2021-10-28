821d7f247efb6b7c8e3a17c7f119b054456a335b
demetreades <demetreades@protonmail.com>
2021-10-28 19:30:09 +0300

    logout moved to header

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,63 +1,35 @@
-import { Link, Route, Switch, useHistory } from 'react-router-dom';
-import { useContext } from 'react'
+import { Route, Switch } from 'react-router-dom';
 
-import Button from '@material-ui/core/Button';
 import UsersTable from '../components/dashboard/UsersTable';
 import ProductsTable from '../components/dashboard/ProductsTable';
 import OrdersTable from '../components/dashboard/OrdersTable';
 
-import { UserContext } from '../context/UserContext'
+import useStyles from '../styles';
 
 const Dashboard = () => {
-	const { user, setUser } = useContext(UserContext);
-	const history = useHistory();
-
-	const handleLogOut = () => {
-		console.log(
-			`LOGOUT :: ${user.name} ======================== `,
-			user
-		);
-
-		localStorage.removeItem('user');
-		setUser({});
-		history.push('/');
-	};
+	const classes = useStyles();
 
 	return (
-		<div>
-			<h1>Dashboard</h1>
-			<Link to="/dashboard/products">Products...</Link>
-			<Link to="/dashboard/users">Users...</Link>
-			<Link to="/dashboard/orders">Orders...</Link>
-			<br />
-			<Link to="/home">Visit our Homepage</Link>
-
-			<Button variant="contained" onClick={handleLogOut}>
-				Logout
-			</Button>
-
-			{/* <ProductsTable title={'Products List'} />
-				<UsersTable title={'Users List'} />
-				<OrdersTable title={'Orders List'} /> */}
-
-			<Switch>
-				<Route
-					path="/dashboard/products"
-					render={(props) => (
-						<ProductsTable {...props} title={'Products List'} />
-					)}
-				/>
-
-				<Route
-					path="/dashboard/users"
-					render={(props) => <UsersTable {...props} title={'Users List'} />}
-				/>
-				<Route
-					path="/dashboard/Orders"
-					render={(props) => <OrdersTable {...props} title={'Orders List'} />}
-				/>
-			</Switch>
-		</div>
+		<>
+			<section className={classes.child}>
+				<Switch>
+					<Route
+						path="/dashboard/products"
+						render={(props) => (
+							<ProductsTable {...props} title={'Products List'} />
+						)}
+					/>
+					<Route
+						path="/dashboard/users"
+						render={(props) => <UsersTable {...props} title={'Users List'} />}
+					/>
+					<Route
+						path="/dashboard/Orders"
+						render={(props) => <OrdersTable {...props} title={'Orders List'} />}
+					/>
+				</Switch>
+			</section>
+		</>
 	);
 };
 
