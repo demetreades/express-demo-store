caf1f03a7056c801673d0e48f182f54daa9def73
demetreades <demetreades@protonmail.com>
2021-10-28 19:27:26 +0300

    header admin links added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useContext } from "react";
 
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
@@ -7,29 +7,34 @@ import { Accessibility } from '@material-ui/icons';
 import { UserContext } from '../context/UserContext';
 import useStyles from '../styles';
 
+import Button from '@material-ui/core/Button';
+
 const Header = () => {
-	const { user } = useContext(UserContext);
+	const history = useHistory();
 
 	const classes = useStyles();
+
+	const { user, setUser } = useContext(UserContext);
+
+	const handleLogOut = () => {
+		console.log(
+			`LOGOUT :: ${user.name} ======================== `,
+			user
+		);
+
+		localStorage.removeItem('user');
+		setUser({});
+		history.push('/');
+	};
 
 	return (
 		<header>
 			<AppBar position="relative">
 				<Toolbar>
-					<Accessibility className={classes.icon} />
 					<Link to="/home">
-						<Typography variant="h6">demo store</Typography>
+						<Accessibility className={classes.icon} />
+						{/* <Typography variant="h6">demo store</Typography> */}
 					</Link>
-					{user.isAdmin ? (
-						<Link
-							to="/dashboard"
-							className={classes.ml5}
-							variant="body2"
-							underline="hover">
-							dashboard
-						</Link>
-					) : ('')
-					}
 
 					{/* <Link
 						to="/login"
@@ -43,7 +48,11 @@ const Header = () => {
 						className={classes.ml5}
 						variant="body2"
 						underline="hover">
-						{user.login ? 'PROFILE' : 'login'}
+						{user.login ? (
+							<Link to="/profile" className={classes.ml5}>
+								Profile
+							</Link>
+						) : 'login'}
 					</Link>
 					{!user.login ? (
 						<Link to="/register" className={classes.ml5}>
@@ -52,6 +61,26 @@ const Header = () => {
 					) : (
 						''
 					)}
+
+					{user && user.isAdmin ? (
+						<>
+							<Link className={classes.ml5} to="/dashboard/products">Products</Link>
+							<Link className={classes.ml5} to="/dashboard/users">Users</Link>
+							<Link className={classes.ml5} to="/dashboard/orders">Orders</Link>
+							{/* <Link to="/home">Visit our Homepage</Link> */}
+						</>
+					) : ('')
+					}
+
+					{user && user.login ? (
+						<Button
+							variant="contained"
+							onClick={handleLogOut}
+							className={classes.ml5}
+						>
+							Logout
+						</Button>
+					) : ('')}
 				</Toolbar>
 			</AppBar>
 		</header>
