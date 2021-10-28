2401baa788735f2a004948a06c31c52d30c65130
demetreades <demetreades@protonmail.com>
2021-10-28 19:25:50 +0300

    Profile added

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,33 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Order from './pages/Order';
 import ProtectedRoute from './utils/ProtectedRoute';
 import Missing404 from './pages/Missing404';
 
 import { useContext } from 'react'
 
+import useStyles from './styles';
+
 const App = () => {
+	const classes = useStyles();
+
 	const { user } = useContext(UserContext);
 
 	return (
 		<>
 			<Router>
 				<Header />
-				<main>
+				<main className={classes.main}>
 					<Switch>
-						<Route path={['/', '/home']} exact component={HomePage} />
-						<Route path="/login" component={Login} />
+						<Route path={['/', '/home']} component={HomePage} exact />
 						<Route path="/register" component={SignUp} />
+						<Route path="/login" component={Login} />
+						<Route path="/profile" component={Profile} />
+						<Route path="/order" component={Order} />
 						<ProtectedRoute
 							path="/dashboard"
 							auth={user.isAdmin}
