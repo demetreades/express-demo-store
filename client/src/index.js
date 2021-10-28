7d08203b7b9e9b40c735eb9900ee68518135529e
demetreades <demetreades@protonmail.com>
2021-10-28 19:27:40 +0300

    CartProvider added

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,14 @@ import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { UserProvider } from './context/UserContext'
+import { CartProvider } from './context/CartContext'
 
 ReactDOM.render(
 	<React.StrictMode>
 		<UserProvider>
-			<App />
+			<CartProvider>
+				<App />
+			</CartProvider>
 		</UserProvider >
 	</React.StrictMode>,
 	document.getElementById('root')
