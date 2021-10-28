bd7bc4c9c4865b70df5a14d464e8455cbc875d5c
demetreades <demetreades@protonmail.com>
2021-10-28 11:46:32 +0300

    footer refactored

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,23 @@
 import { Typography } from '@material-ui/core';
-
 import useStyles from '../styles';
 
 const Footer = () => {
 	const classes = useStyles();
 
 	return (
-		<>
-			<footer className={classes.footer}>
-				<Typography variant="h6" align="center" gutterBottom>
-					demo-store example footer
-				</Typography>
-				<Typography
-					variant="subtitle1"
-					align="center"
-					color="textSecondary"
-					gutterBottom>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-					facere quia sunt.
-				</Typography>
-			</footer>
-		</>
+		<footer className={classes.footer}>
+			<Typography variant="h6" align="center" gutterBottom>
+				demo-store example footer
+			</Typography>
+			<Typography
+				variant="subtitle1"
+				align="center"
+				color="textSecondary"
+				gutterBottom>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+				facere quia sunt.
+			</Typography>
+		</footer>
 	);
 };
 
