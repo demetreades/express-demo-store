d48fd6e9c9c4448a5af878bcfee3eecfb5cac485
demetreades <demetreades@protonmail.com>
2021-10-29 23:01:01 +0300

    console.log removed

--- server/src/utils/upload.js
+++ server/src/utils/upload.js
@@ -1,17 +1,14 @@
 const path = require('path');
 const multer = require('multer');
 
-const logger = require('./logger');
-
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
 		cb(null, 'client/public/img/products');
 	},
 	filename: (req, file, cb) => {
 		const fileName = Date.now() + path.extname(file.originalname);
-		logger.warn(file);
-		logger.warn(fileName);
 		req.fileName = fileName;
+		req.file = file;
 		cb(null, fileName);
 	}
 });
