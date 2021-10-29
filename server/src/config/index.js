451d43de766a8b916a232c289160fbf45e3e1682
demetreades <demetreades@protonmail.com>
2021-10-29 20:48:41 +0300

    ++

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,6 +2,6 @@ const { corsOptions } = require('./cors/corsOptions');
 const { limitOptions } = require('./limitOptions');
 
 module.exports = {
-  corsOptions,
-  limitOptions,
+	corsOptions,
+	limitOptions,
 };
