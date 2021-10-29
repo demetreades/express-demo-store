708d92581e95f3ac5344e8776dda3364bdb03675
demetreades <demetreades@protonmail.com>
2021-10-29 23:03:36 +0300

    morgan, handleMulterErrors added

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const compression = require('compression');
 
 const { limitOptions, corsOptions } = require('./config');
-const { handleErrors, handleMongoErrors, handleNotFound } = require('./utils');
+const { handleErrors, handleMongoErrors, handleMulterErrors, handleNotFound } = require('./utils');
 const { userRoutes, orderRoutes, productRoutes } = require('./routes');
 const dbConnection = require('./services/db/connection');
 
@@ -15,19 +16,22 @@ dbConnection();
 
 const app = express();
 
+app.use(morgan('dev'));
+
 app.use(helmet());
 app.use(rateLimit(limitOptions));
 app.use(cors(corsOptions));
 app.use(compression());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use('/users', userRoutes);
 app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
 
 app.use(handleNotFound);
+app.use(handleMulterErrors);
 app.use(handleMongoErrors);
 app.use(handleErrors);
 
