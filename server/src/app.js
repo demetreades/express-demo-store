3b936f1025c33f1081a7e101f068579fe800ef48
demetreades <demetreades@protonmail.com>
2021-11-04 20:52:08 +0200

    xss-clean added

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean');
 const compression = require('compression');
 
 const { limitOptions, corsOptions } = require('./config');
@@ -18,10 +19,11 @@ const app = express();
 app.use(helmet());
 app.use(rateLimit(limitOptions));
 app.use(cors(corsOptions));
+app.use(xss());
 app.use(compression());
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRoutes);
 app.use('/orders', orderRoutes);
